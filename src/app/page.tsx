8dfbc91709715d3d9e225635b6f77d57391322a9
Dmitrii Kolca <leonidi4@list.ru>
2024-12-09 11:48:28 +0200

    Add submodule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import {variable1, variable2, variable3, variable4} from "../../library/src/lib";
 
 export default function Home() {
   return (
@@ -13,38 +14,12 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <div>{variable1}</div>
+        <div>{variable2}</div>
+        <div>{variable3}</div>
+        <div>{variable4}</div>
+
       </main>
       <footer className={styles.footer}>
         <a
